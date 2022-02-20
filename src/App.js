ec8d265189d53cbc1eac826d38c2f3e22169234f
Naman Saxena <90456667+Naman-Saxena1@users.noreply.github.com>
2022-02-20 18:53:53 +0530

    feat : Added error handling

--- src/App.js
+++ src/App.js
@@ -3,24 +3,42 @@ import axios from "axios";
 import "./styles.css";
 
 export default function App() {
-  let [loader, setLoader] = useState(true);
+  let [loader, setLoader] = useState(false);
   let [data, setData] = useState([]);
+  let [errorMsg, setErrorMsg] = useState(false);
 
   useEffect(() => {
     (async function getDataFunc() {
-      await axios.get("/api/products").then((response) => {
-        setLoader(false);
-        setData(response.data.products);
-      });
+      setLoader(true);
+      await axios
+        .get("/api/products")
+        .then((response) => {
+          setLoader(false);
+          setErrorMsg(false);
+          setData(response.data.products);
+        })
+        .catch((error) => {
+          setLoader(false);
+          setErrorMsg(true);
+        });
     })();
   }, []);
 
   return (
     <div className="App">
       <h1> Showcase Products </h1>
+      {loader ? <h2>Loading...</h2> : ""}
+      {errorMsg ? <h2>Something Went Wrong! :(</h2> : ""}
       <ol>
         {data.map((item) => (
-          <li id={item.id}>{item.name}</li>
+          <li id={item.id}>
+            <img
+              style={{ width: "300px", height: "200px" }}
+              src={item.image}
+              alt={item.name}
+            />
+            <p>{item.name}</p>
+          </li>
         ))}
       </ol>
     </div>
